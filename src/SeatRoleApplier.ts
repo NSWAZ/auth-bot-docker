92ae51aeae926473f401893c29f06f954bde106a
goemktg <samktg52@gmail.com>
2024-02-29 04:23:35 +0900

    오류 수정, 라이브러리화 fix: SeatRoleApplier.getSeatUserId 가 userId가 아닌 main_character_id를 반환하는 문제 수정 feat: 뉴비 롤 반영 라이브러리화

--- src/SeatRoleApplier.ts
+++ src/SeatRoleApplier.ts
@@ -33,7 +33,7 @@ export class SeatRoleApplier {
 			seatUserIndex++;
 		}
 
-		return user.main_character_id;
+		return user.id.toString();
 	}
 
 	/**
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import { Client, GatewayIntentBits, Events, AuditLogEvent } from 'discord.js';
-import { getAuditTargetNickname, loadEnvironmentVariables } from './library/functions';
+import { getAuditTargetNickname, loadEnvironmentVariables, reflectNewbieRoleChange } from './library/functions';
 import { SeatRoleApplier } from './SeatRoleApplier';
 import { CommandsHandler } from './library/handlers/Commands';
-import { DiscordRole } from './library/types';
 
 loadEnvironmentVariables();
 
-// const seatUsersCache = new Map<string, string>();
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildMembers] });
 const commandsHandler = new CommandsHandler();
 
@@ -26,20 +24,17 @@ client.on(Events.InteractionCreate, interaction => {
 		.catch(console.error);
 });
 
-// TODO : 롤 여러개 감시 가능하게 수정
 client.on(Events.GuildAuditLogEntryCreate, async (auditLog, guild) => {
 	if (auditLog.action != AuditLogEvent.MemberRoleUpdate || auditLog.executorId === '1066230195473883136') return;
 
-	const watchRoleIds = ['1210191232756621383', '1210112780141600818'];
 	const nickname = await getAuditTargetNickname(auditLog, guild);
-
-	for (const change of auditLog.changes) {
-		for (const newRole of (change.new as DiscordRole[])) {
-			if (watchRoleIds.includes(newRole.id)) {
-
-				if (change.key === '$add') {await client.seatRoleApllier.add(nickname);}
-				else if (change.key === '$remove') {await client.seatRoleApllier.remove(nickname);}
-			}
-		}
-	}
+	void reflectNewbieRoleChange(auditLog, nickname, add, remove);
 });
+
+function add(nickname: string) {
+	void client.seatRoleApllier.add(nickname);
+}
+
+function remove(nickname: string) {
+	void client.seatRoleApllier.remove(nickname);
+}

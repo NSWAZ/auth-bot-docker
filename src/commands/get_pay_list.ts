f78e874bf6421c057f3194c8b471acacce4ab45a
goemktg <samktg52@gmail.com>
2025-06-01 07:16:28 +0000

    코드 스타일 개선 및 불필요한 주석 제거 - `functionApplyInactives.ts` 파일에서 코드 포맷팅 및 주석 정리 - `get_pay_list.ts` 파일에서 코드 정리 및 가독성 향상 - `inactive.ts` 파일에서 불필요한 공백 제거

--- src/commands/get_pay_list.ts
+++ src/commands/get_pay_list.ts
@@ -6,7 +6,6 @@ import {
 } from "discord.js";
 import { SlashCommand } from "../library/types";
 import { EsiRequester } from "../library/classes/EsiHandler";
-import { DatabaseEngine } from "../library/classes/DatabaseEngine";
 
 const GetPayListCommand: SlashCommand = {
   command: new SlashCommandBuilder()
@@ -30,50 +29,50 @@ const GetPayListCommand: SlashCommand = {
     const query = "SELECT * FROM srp_records WHERE status_string = 'approved'";
 
     void (async () => {
-        if (interaction.client.databaseEngine === undefined)
-          throw new Error("DatabaseEngine is not initd");
+      if (interaction.client.databaseEngine === undefined)
+        throw new Error("DatabaseEngine is not initd");
 
-        const srp_records = await interaction.client.databaseEngine.query(query);
+      const srp_records = await interaction.client.databaseEngine.query(query);
 
-        if (srp_records.rowCount === 0) {
-          void interaction.reply("아직 승인된 SRP 신청이 없습니다.");
-          return;
-        } else {
-          const pay_list: Record<number, number> = {};
-          for (const record of srp_records.rows as {
-            main_char_id: number;
-            amount: number;
-          }[]) {
-            const amount = Math.ceil(record.amount);
+      if (srp_records.rowCount === 0) {
+        void interaction.reply("아직 승인된 SRP 신청이 없습니다.");
+        return;
+      } else {
+        const pay_list: Record<number, number> = {};
+        for (const record of srp_records.rows as {
+          main_char_id: number;
+          amount: number;
+        }[]) {
+          const amount = Math.ceil(record.amount);
 
-            if (pay_list[record.main_char_id] === undefined) {
-              pay_list[record.main_char_id] = amount;
-            } else {
-              pay_list[record.main_char_id] += amount;
-            }
+          if (pay_list[record.main_char_id] === undefined) {
+            pay_list[record.main_char_id] = amount;
+          } else {
+            pay_list[record.main_char_id] += amount;
           }
-
-          const names = await EsiRequester.getNamesFromIds(
-            Object.keys(pay_list).map(Number),
-          );
-
-          let message = "SRP 신청이 승인된 캐릭터들의 금액입니다.\n```";
-          for (const [main_char_id, amount] of Object.entries(pay_list)) {
-            const name = names.find(
-              (name) => name.id === Number(main_char_id),
-            )?.name;
-            message += `${name} \n${amount.toLocaleString()} ISK\n\n`;
-          }
-
-          void interaction.reply({
-            content: message + "```",
-            components: [row],
-          });
-
-          void interaction.client.databaseEngine.query(
-            "UPDATE srp_records SET status_string = 'wait_paid' WHERE status_string = 'approved'",
-          );
         }
+
+        const names = await EsiRequester.getNamesFromIds(
+          Object.keys(pay_list).map(Number),
+        );
+
+        let message = "SRP 신청이 승인된 캐릭터들의 금액입니다.\n```";
+        for (const [main_char_id, amount] of Object.entries(pay_list)) {
+          const name = names.find(
+            (name) => name.id === Number(main_char_id),
+          )?.name;
+          message += `${name} \n${amount.toLocaleString()} ISK\n\n`;
+        }
+
+        void interaction.reply({
+          content: message + "```",
+          components: [row],
+        });
+
+        void interaction.client.databaseEngine.query(
+          "UPDATE srp_records SET status_string = 'wait_paid' WHERE status_string = 'approved'",
+        );
+      }
     })();
   },
 };
